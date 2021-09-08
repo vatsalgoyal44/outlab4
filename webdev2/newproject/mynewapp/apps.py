ac9e9869580b30b6bdc5ed88079e473aa3423569
Vatsal Goyal <vatsalgoyal44@gmail.com>
2021-09-08 12:41:44 +0530

    signup page done

--- webdev2/newproject/mynewapp/apps.py
+++ webdev2/newproject/mynewapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class NewappConfig(AppConfig):
+class MynewappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'newapp'
+    name = 'mynewapp'
