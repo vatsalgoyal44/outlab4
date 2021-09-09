b24493a84701a4df78fd6015e58015729a87f931
Vatsal Goyal <vatsalgoyal44@gmail.com>
2021-09-09 14:46:56 +0530

    updated profiles

--- webdev2/newproject/accounts/models.py
+++ webdev2/newproject/accounts/models.py
@@ -5,9 +5,13 @@ from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
     user =                      models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name =                models.CharField(('first name'), max_length=30, blank=False)
     followernumber =            models.IntegerField(default=0)
-    lastupdate =                models.DateTimeField(blank = True)
+    lastupdate =                models.DateTimeField(blank = True, null = True)
+    title = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.title
+
 
 class Repository(models.Model):
     owner =                     models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -16,6 +20,6 @@ class Repository(models.Model):
 
 def create_profile(sender, **kwargs):
     if kwargs['created']:
-        user_profile = Profile.objects.create(user=kwargs['instance'])
+        user_profile = Profile.objects.create(user=kwargs['instance'], title=kwargs['instance'])
 
 post_save.connect(create_profile, sender=User)        
