9efa6f7b57acc1cd7bf8553f60c46c077f9b0d32
Vatsal Goyal <vatsalgoyal44@gmail.com>
2021-09-12 10:35:20 +0530

    FETCH completed

--- webdev2/newproject/accounts/admin.py
+++ webdev2/newproject/accounts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Profile, Repository
+from django.db import models
+
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(Repository)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user','followernumber', 'date_updated', 'title')
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('repname','owner', 'starrating')
+
+
+
+admin.site.register(Profile,ProfileAdmin)
+admin.site.register(Repository, RepositoryAdmin)
--- webdev2/newproject/accounts/models.py
+++ webdev2/newproject/accounts/models.py
@@ -1,25 +1,51 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+import requests
+import os
+from urllib.parse import urljoin
+from django.utils import timezone
+
+
 
 # Create your models here.
 class Profile(models.Model):
     user =                      models.OneToOneField(User, on_delete=models.CASCADE)
-    followernumber =            models.IntegerField(default=0)
-    lastupdate =                models.DateTimeField(blank = True, null = True)
-    title = models.CharField(max_length=100, default='')
-
+    followernumber =            models.IntegerField(blank = True, null = True)
+    date_updated =              models.DateTimeField(auto_now=True)
+    title =                     models.CharField(max_length=100, null = True)
     def __str__(self):
-        return self.title
-
+            return str(self.title)
 
 class Repository(models.Model):
-    owner =                     models.ForeignKey(Profile, on_delete=models.CASCADE)
-    repname =                   models.CharField(max_length=100, default='')
-    starrating =                models.IntegerField(default=0)
+    repname =                   models.CharField(max_length=100, default='', null=True, blank = True)
+    starrating =                models.IntegerField(default=0, null = True, blank = True)
+    owner =                     models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
+    
+    def __str__(self):
+        return self.repname
+    class Meta:
+        # Gives the proper plural name for admin
+        verbose_name_plural = "Repositories"
+
+
+def create_repository(sender, **kwargs):
+    if kwargs['created']:
+        str = urljoin('https://api.github.com/users/',kwargs['instance'].title+'/')
+        responserepo = requests.get(urljoin(str,'repos'))
+        drepo = responserepo.json()
+        for i in range(len(drepo)):
+            rep = Repository.objects.create(owner = kwargs['instance'], repname=drepo[i]['name'], starrating = drepo[i]['stargazers_count'])
 
 def create_profile(sender, **kwargs):
     if kwargs['created']:
-        user_profile = Profile.objects.create(user=kwargs['instance'], title=kwargs['instance'])
+        response = requests.get(urljoin('https://api.github.com/users/',kwargs['instance'].username))
+        d = response.json()
+        user_profile = Profile.objects.create(user=kwargs['instance'], followernumber = int(d['followers']), title = kwargs['instance'].username)
+        user_profile.save()
 
-post_save.connect(create_profile, sender=User)        
+        
+post_save.connect(create_profile, sender=User)
+post_save.connect(create_repository, sender = Profile)
+
